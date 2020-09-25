da75ebe6459c362dbeb7483e496e8830af08e44e
Showrabh Dhar <ga5982@wayne.edu>
2020-09-24 21:28:44 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-//Author: Amiangshu Bosu <amiangshu.bosu@wayne.edu>
+//Author: Showrabh Dhar <ga5982@wayne.edu>
   
 // Header file for input output functions 
 #include<iostream>  
